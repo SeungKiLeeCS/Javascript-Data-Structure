c96102d98928e6314d111a2242bb50b024b9d438
Seung Ki Lee <seungkilee92@gmail.com>
2018-12-27 16:14:29 -0600

    restructuring + added visualizer

---
+++ visualizer/src/views/App.ko.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class AppKo extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          자료구조 시각화 페이지
+        </header>
+      </div>
+    );
+  }
+}
+
+export default AppKo;
