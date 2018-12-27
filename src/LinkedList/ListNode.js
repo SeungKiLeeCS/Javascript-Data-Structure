bf45ff351bbe59e232cb9316c16d514741d100e1
SeungKiLee <seungkil@smu.edu>
2018-12-27 00:21:23 -0600

    test fixes

--- src/LinkedList/ListNode.js
+++ src/LinkedList/ListNode.js
@@ -1,12 +1,12 @@
 export default class ListNode {
     
     constructor(val) {
-        if (val) {
+        if (val != undefined) {
             this.val = val;
             this.next = null;  
-            this.prev = null;      
+            this.prev = null;    
         } else {
-            console.log("Please input a value.");
+            console.log("Undefined Value")
         }
     }
 }
---
+++ src/LinkedList/__test__/ListNode.test.js
@@ -0,0 +1,15 @@
+import ListNode from '../ListNode';
+
+var expect = require('expect.js');
+
+describe('List Node', () => {
+    // Happy Path
+    it('should create a node with value and pointers', () => {
+        const node = new ListNode(0);
+
+        expect(node.val).to.be(0);
+        expect(node.prev).to.be(null);
+        expect(node.next).to.be(node.prev);
+    });
+    
+});
